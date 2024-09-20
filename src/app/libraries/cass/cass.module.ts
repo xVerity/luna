339be90ca6cd686467231b333283c1f21c286694
Michael Schwab <michael.schwab@accantum.de>
2024-09-20 09:39:53 +0200

    Add CassModule and LilyModule
    This commit adds two new modules, CassModule and LilyModule, to the application.
    
    CassModule provides a set of common utilities for use in Angular applications, while LilyModule provides a set of common components and directives.
    
    It is recommended not to import these modules directly, but instead import the individual utilities, components, and directives needed in the application.

---
+++ src/app/libraries/cass/cass.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+/**
+ * The Cassandra utility framework provides a set of common utilities for use in Angular applications.
+ * It is recommended not to import this module directly but instead import the individual utilities
+ * needed in your application.
+ */
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [],
+})
+export class CassModule {}
---
+++ src/app/libraries/lily/lily.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+/**
+ * The Lily component framework provides a set of common components and directives for use
+ * in Angular applications.
+ *
+ * It is recomennded not to import this module directly but instead import the individual
+ * components and directives that you need in your application.
+ */
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [],
+})
+export class LilyModule {}
